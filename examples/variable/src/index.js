f4e0d10285ba0c15806fd2459f6407355e4a195b
Tanner Linsley <tannerlinsley@gmail.com>
2020-05-11 17:31:19 -0600

    chore: fix examples

--- examples/variable/src/index.js
+++ examples/variable/src/index.js
@@ -3,39 +3,36 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-import Fixed from "./Fixed";
-import Variable from "./Variable";
-import Dynamic from "./Dynamic";
-import SmoothScroll from "./SmoothScroll";
-import InfiniteScroll from "./InfiniteScroll";
+import { useVirtual } from "react-virtual";
 
 function App() {
-  const [mode, setMode] = React.useState("fixed"); // fixed | variable | dynamic
+  const rows = new Array(10000)
+    .fill(true)
+    .map(() => 25 + Math.round(Math.random() * 100));
+
+  const columns = new Array(10000)
+    .fill(true)
+    .map(() => 75 + Math.round(Math.random() * 100));
 
   return (
     <div>
-      <select value={mode} onChange={e => setMode(e.target.value)}>
-        <option value="fixed">Fixed</option>
-        <option value="variable">Variable</option>
-        <option value="dynamic">Dynamic</option>
-        <option value="smooth-scroll">Smooth Scroll</option>
-        <option value="infinite-scroll">Infinite Scroll</option>
-      </select>
-
+      <p>
+        These components are using <strong>variable</strong> sizes. This means
+        that each element has a unique, but knowable dimension at render time.
+      </p>
       <br />
       <br />
 
-      {mode === "fixed" ? (
-        <Fixed />
-      ) : mode === "variable" ? (
-        <Variable />
-      ) : mode === "dynamic" ? (
-        <Dynamic />
-      ) : mode === "smooth-scroll" ? (
-        <SmoothScroll />
-      ) : mode === "infinite-scroll" ? (
-        <InfiniteScroll />
-      ) : null}
+      <h3>Rows</h3>
+      <RowVirtualizerVariable rows={rows} columns={columns} />
+      <br />
+      <br />
+      <h3>Columns</h3>
+      <ColumnVirtualizerVariable rows={rows} columns={columns} />
+      <br />
+      <br />
+      <h3>Grid</h3>
+      <GridVirtualizerVariable rows={rows} columns={columns} />
       <br />
       <br />
       {process.env.NODE_ENV === "development" ? (
@@ -46,6 +43,179 @@ function App() {
         </p>
       ) : null}
     </div>
+  );
+}
+
+function RowVirtualizerVariable({ rows }) {
+  const parentRef = React.useRef();
+
+  const rowVirtualizer = useVirtual({
+    size: rows.length,
+    parentRef,
+    estimateSize: React.useCallback(i => rows[i], [rows]),
+    overscan: 5
+  });
+
+  return (
+    <>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          height: `200px`,
+          width: `400px`,
+          overflow: "auto"
+        }}
+      >
+        <div
+          style={{
+            height: `${rowVirtualizer.totalSize}px`,
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          {rowVirtualizer.virtualItems.map(virtualRow => (
+            <div
+              key={virtualRow.index}
+              className={virtualRow.index % 2 ? "ListItemOdd" : "ListItemEven"}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: `${virtualRow.size}px`,
+                transform: `translateY(${virtualRow.start}px)`
+              }}
+            >
+              Row {virtualRow.index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function ColumnVirtualizerVariable({ columns }) {
+  const parentRef = React.useRef();
+
+  const columnVirtualizer = useVirtual({
+    horizontal: true,
+    size: columns.length,
+    parentRef,
+    estimateSize: React.useCallback(i => columns[i], [columns]),
+    overscan: 5
+  });
+
+  return (
+    <>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          width: `400px`,
+          height: `100px`,
+          overflow: "auto"
+        }}
+      >
+        <div
+          style={{
+            width: `${columnVirtualizer.totalSize}px`,
+            height: "100%",
+            position: "relative"
+          }}
+        >
+          {columnVirtualizer.virtualItems.map(virtualColumn => (
+            <div
+              key={virtualColumn.index}
+              className={
+                virtualColumn.index % 2 ? "ListItemOdd" : "ListItemEven"
+              }
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                height: "100%",
+                width: `${virtualColumn.size}px`,
+                transform: `translateX(${virtualColumn.start}px)`
+              }}
+            >
+              Column {virtualColumn.index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function GridVirtualizerVariable({ rows, columns }) {
+  const parentRef = React.useRef();
+
+  const rowVirtualizer = useVirtual({
+    size: rows.length,
+    parentRef,
+    estimateSize: React.useCallback(i => rows[i], [rows]),
+    overscan: 5
+  });
+
+  const columnVirtualizer = useVirtual({
+    horizontal: true,
+    size: columns.length,
+    parentRef,
+    estimateSize: React.useCallback(i => columns[i], [columns]),
+    overscan: 5
+  });
+
+  return (
+    <>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          height: `400px`,
+          width: `500px`,
+          overflow: "auto"
+        }}
+      >
+        <div
+          style={{
+            height: `${rowVirtualizer.totalSize}px`,
+            width: `${columnVirtualizer.totalSize}px`,
+            position: "relative"
+          }}
+        >
+          {rowVirtualizer.virtualItems.map(virtualRow => (
+            <React.Fragment key={virtualRow.index}>
+              {columnVirtualizer.virtualItems.map(virtualColumn => (
+                <div
+                  key={virtualColumn.index}
+                  className={
+                    virtualColumn.index % 2
+                      ? virtualRow.index % 2 === 0
+                        ? "ListItemOdd"
+                        : "ListItemEven"
+                      : virtualRow.index % 2
+                      ? "ListItemOdd"
+                      : "ListItemEven"
+                  }
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: `${virtualColumn.size}px`,
+                    height: `${virtualRow.size}px`,
+                    transform: `translateX(${virtualColumn.start}px) translateY(${virtualRow.start}px)`
+                  }}
+                >
+                  Cell {virtualRow.index}, {virtualColumn.index}
+                </div>
+              ))}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
