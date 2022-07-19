eab7320998e36cdea34aef0c74af7fca4eae5ed4
Kenneth Gitere <gitere81@gmail.com>
2022-07-20 00:06:05 +0300

    feat: Add Svelte Virtual Adapter (#317)

---
+++ packages/svelte-virtual/src/index.ts
@@ -0,0 +1,66 @@
+import {
+  elementScroll,
+  observeElementOffset,
+  observeElementRect,
+  observeWindowOffset,
+  observeWindowRect,
+  PartialKeys,
+  Virtualizer,
+  VirtualizerOptions,
+  windowScroll,
+} from '@tanstack/virtual-core'
+export * from '@tanstack/virtual-core'
+
+import { readable, Readable } from 'svelte/store'
+
+function createVirtualizerBase<TScrollElement, TItemElement = unknown>(
+  options: VirtualizerOptions<TScrollElement, TItemElement>,
+): Readable<Virtualizer<TScrollElement, TItemElement>> {
+  const virtualizer = new Virtualizer(options)
+
+  return readable(virtualizer, (set) => {
+    const cleanup = virtualizer._didMount()
+    virtualizer._willUpdate()
+    virtualizer.setOptions({
+      ...options,
+      onChange: (instance: Virtualizer<TScrollElement, TItemElement>) => {
+        set(instance)
+        options.onChange?.(instance)
+      },
+    })
+
+    return () => cleanup()
+  })
+}
+
+export function createVirtualizer<TScrollElement, TItemElement = unknown>(
+  options: PartialKeys<
+    VirtualizerOptions<TScrollElement, TItemElement>,
+    'observeElementRect' | 'observeElementOffset' | 'scrollToFn'
+  >,
+): Readable<Virtualizer<TScrollElement, TItemElement>> {
+  return createVirtualizerBase<TScrollElement, TItemElement>({
+    observeElementRect: observeElementRect,
+    observeElementOffset: observeElementOffset,
+    scrollToFn: elementScroll,
+    ...options,
+  })
+}
+
+export function createWindowVirtualizer<TItemElement = unknown>(
+  options: PartialKeys<
+    VirtualizerOptions<Window, TItemElement>,
+    | 'getScrollElement'
+    | 'observeElementRect'
+    | 'observeElementOffset'
+    | 'scrollToFn'
+  >,
+): Readable<Virtualizer<Window, TItemElement>> {
+  return createVirtualizerBase<Window, TItemElement>({
+    getScrollElement: () => (typeof window !== 'undefined' ? window : null!),
+    observeElementRect: observeWindowRect,
+    observeElementOffset: observeWindowOffset,
+    scrollToFn: windowScroll,
+    ...options,
+  })
+}
