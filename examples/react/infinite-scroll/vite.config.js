927d97aafd8d3a233a0603be78522083062ea4a4
Tanner Linsley <tannerlinsley@gmail.com>
2022-07-19 21:38:43 -0600

    examples: enable production bundling

--- examples/react/infinite-scroll/vite.config.js
+++ examples/react/infinite-scroll/vite.config.js
@@ -5,14 +5,5 @@ import rollupReplace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    rollupReplace({
-      preventAssignment: true,
-      values: {
-        __DEV__: JSON.stringify(true),
-        'process.env.NODE_ENV': JSON.stringify('development'),
-      },
-    }),
-    react(),
-  ],
+  plugins: [react()],
 })
