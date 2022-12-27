3124c39e4847bc21ea9da4936033f07ffc09800c
Damian Pieczynski <piecyk@gmail.com>
2022-12-27 08:54:54 +0100

    Make scrollOffset public, add scrollBy (#456)
    * refactor: make scrollOffset public
    
    * feat: add scrollBy

--- packages/virtual-core/src/index.ts
+++ packages/virtual-core/src/index.ts
@@ -289,7 +289,7 @@ export class Virtualizer<
   private itemMeasurementsCache: Record<Key, number> = {}
   private pendingMeasuredCacheIndexes: number[] = []
   private scrollRect: Rect
-  private scrollOffset: number
+  scrollOffset: number
   private scrollAdjustments: number = 0
   private measureElementCache: Record<Key, TItemElement> = {}
   private pendingScrollToIndexCallback: (() => void) | null = null
@@ -695,6 +695,14 @@ export class Virtualizer<
         }
       }
     }
+  }
+
+  scrollBy = (adjustments: number, options?: { behavior: ScrollBehavior }) => {
+    this._scrollToOffset(this.scrollOffset, {
+      adjustments,
+      behavior: options?.behavior,
+      sync: false,
+    })
   }
 
   getTotalSize = () =>
