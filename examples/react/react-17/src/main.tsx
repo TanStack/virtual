8ee027fdf3dd64a4a71aef144cacadf366a9d68a
Alissa <alissa.vrk@gmail.com>
2023-02-18 17:12:46 +0200

    add react 17 example

---
+++ examples/react/react-17/src/main.tsx
@@ -0,0 +1,66 @@
+import './index.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { useVirtualizer } from '@tanstack/react-virtual'
+
+const App = () => {
+  const parentRef = React.useRef(null)
+
+  const rowVirtualizer = useVirtualizer({
+    count: 10000,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+    overscan: 5,
+  })
+
+  return (
+    <>
+      <p>
+        this example demostrates the use of the virtualizer with react 17.
+        Basically, all you need to do is to install <code>use-sync-external-store</code> polyfill
+      </p>
+      <br></br>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          height: `200px`,
+          width: `400px`,
+          overflow: 'auto',
+        }}
+      >
+        <div
+          style={{
+            height: `${rowVirtualizer.totalSize}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {rowVirtualizer.virtualItems.map((virtualRow) => (
+            <div
+              key={virtualRow.index}
+              className={virtualRow.index % 2 ? 'ListItemOdd' : 'ListItemEven'}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualRow.size}px`,
+                transform: `translateY(${virtualRow.start}px)`,
+              }}
+            >
+              Row {virtualRow.index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
