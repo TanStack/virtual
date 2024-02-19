d3a50ce7f9008a95da5d0a1ac30727a55577c2ff
bluejoyq <bluejoy32@gmail.com>
2024-02-19 23:26:08 +0900

    feat: example draft

--- examples/react/two-way-infinite-scroll/src/main.tsx
+++ examples/react/two-way-infinite-scroll/src/main.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { QueryClient, QueryClientProvider, useInfiniteQuery } from '@tanstack/react-query'
+import { InfiniteData, QueryClient, QueryClientProvider, UseInfiniteQueryResult, useInfiniteQuery } from '@tanstack/react-query'
 import './index.css'
-import { useVirtualizer } from '@tanstack/react-virtual'
+import { useWindowVirtualizer } from '@tanstack/react-virtual'
 
 const queryClient = new QueryClient()
 
+interface Page {
+  rows:string[],
+  nextOffset: number
+}
 async function fetchServerPage(
   limit: number,
   offset: number = 0,
-): Promise<{ rows: string[]; nextOffset: number }> {
+): Promise<Page> {
   const rows = new Array(limit)
     .fill(0)
     .map((e, i) => `Async loaded row #${i + offset * limit}`)
@@ -20,93 +24,110 @@ async function fetchServerPage(
 }
 
 function App() {
+  const SCROLL_MARGIN = 200;
+  const MAX_PAGE_LENGTH = 5;
+  const ITEM_HEIGHT = 100;
+  const [dataPerPagePrefixSum, setDataPerPagePrefixSum] = useState<number[]>([0]);
   const {
     status,
     data,
     error,
-    isFetching,
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    
+    isFetchingPreviousPage,
+    fetchPreviousPage,
+    hasPreviousPage,
+  } : UseInfiniteQueryResult<InfiniteData<Page,  number>, Error>= useInfiniteQuery(
     {
       queryKey:['projects'],
-      queryFn:(ctx) => fetchServerPage(10, ctx.pageParam),
+      queryFn:async ({pageParam}) => {
+        const res = await fetchServerPage(10, pageParam)
+        if(dataPerPagePrefixSum.length <= pageParam + 1){
+          setDataPerPagePrefixSum((prev) => [...prev,(prev.slice(-1)[0] ?? 0) + res.rows.length])
+        }
+        return res
+      },
       initialPageParam:0,
-      getNextPageParam: (_lastGroup, groups) => groups.length,
+      getNextPageParam: (_, __, lastPageParam) => {
+        return lastPageParam + 1;
+      },
+      getPreviousPageParam: (_, __, firstPageParam) => {
+        if (firstPageParam <= 0) {
+          return null;
+        }
+        return firstPageParam - 1;
+      },
+      maxPages: MAX_PAGE_LENGTH,
     },
   )
 
-  const allRows = data ? data.pages.flatMap((d) => d.rows) : []
-
-  const parentRef = React.useRef<HTMLDivElement | null>(null)
-
-  const rowVirtualizer = useVirtualizer({
-    count: hasNextPage ? allRows.length + 1 : allRows.length,
-    getScrollElement: () => parentRef.current,
-    estimateSize: () => 100,
+  const allRows = data ? data.pages.flatMap((d) => d.rows) : [];
+  const minPageParam = data?.pageParams[0] ?? 0;
+  // 메모리 최적화를 위해 없어진 데이터의 갯수
+  const minPageDataLength = (dataPerPagePrefixSum[minPageParam] ?? 0);
+  const maxDataLength = dataPerPagePrefixSum.slice(-1)[0] ?? 0;
+  const rowVirtualizer = useWindowVirtualizer({
+    count: maxDataLength,
+    estimateSize: () => ITEM_HEIGHT,
     overscan: 5,
+    scrollMargin: SCROLL_MARGIN
   })
+  const virtualItems = rowVirtualizer.getVirtualItems()
 
+  const handleScroll = useCallback((): void => {
+    // 전체 문서의 높이
+    const scrollHeight = document.documentElement.scrollHeight;
+    // 현재 스크롤 위치
+    const scrollTop = document.documentElement.scrollTop;
+    // 뷰포트 높이
+    const clientHeight = document.documentElement.clientHeight;
+    // 남은 스크롤 높이 계산
+    const scrollRemaining = scrollHeight - scrollTop - clientHeight;
+    // 남은 스크롤이 아이템 2개 미만이면 다음 데이터 fetch
+    if (scrollRemaining < ITEM_HEIGHT * 2 && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+    // 남은 스크롤이 아이템 2개 미만이면 이전 데이터 fetch
+    if (scrollTop-SCROLL_MARGIN < (minPageDataLength - 2) * ITEM_HEIGHT && hasPreviousPage && !isFetchingPreviousPage) {
+      fetchPreviousPage();
+    }
+    
+  }, [hasPreviousPage,isFetchingPreviousPage,hasNextPage,isFetchingNextPage,minPageDataLength])
   React.useEffect(() => {
-    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse()
-
-    if (!lastItem) {
-      return
+    document.addEventListener('scroll',handleScroll);
+    return ()=>{
+      document.removeEventListener('scroll',handleScroll)
     }
-
-    if (
-      lastItem.index >= allRows.length - 1 &&
-      hasNextPage &&
-      !isFetchingNextPage
-    ) {
-      fetchNextPage()
-    }
-  }, [
-    hasNextPage,
-    fetchNextPage,
-    allRows.length,
-    isFetchingNextPage,
-    rowVirtualizer.getVirtualItems(),
-  ])
-
+  }, [handleScroll])
   return (
     <div>
-      <p>
+      <p style={{
+        height:SCROLL_MARGIN,
+      }}>
         This infinite scroll example uses React Query's useInfiniteScroll hook
         to fetch infinite data from a posts endpoint and then a rowVirtualizer
         is used along with a loader-row placed at the bottom of the list to
         trigger the next page to load.
       </p>
 
-      <br />
-      <br />
-
       {status === 'pending' ? (
         <p>Loading...</p>
       ) : status === 'error' ? (
         <span>Error: {(error as Error).message}</span>
       ) : (
-        <div
-          ref={parentRef}
-          className="List"
-          style={{
-            height: `500px`,
-            width: `100%`,
-            overflow: 'auto',
-          }}
-        >
+
           <div
             style={{
               height: `${rowVirtualizer.getTotalSize()}px`,
               width: '100%',
-              position: 'relative',
+              display:'flex',
+              flexDirection:'column',
+              gap:'16px'
             }}
           >
-            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-              const isLoaderRow = virtualRow.index > allRows.length - 1
-              const post = allRows[virtualRow.index]
+            {virtualItems.map((virtualRow) => {
+              const post = allRows[virtualRow.index - minPageDataLength]
 
               return (
                 <div
@@ -123,29 +144,12 @@ function App() {
                     transform: `translateY(${virtualRow.start}px)`,
                   }}
                 >
-                  {isLoaderRow
-                    ? hasNextPage
-                      ? 'Loading more...'
-                      : 'Nothing more to load'
-                    : post}
+                  {post == null ? 'loading' : post}
                 </div>
               )
             })}
           </div>
-        </div>
       )}
-      <div>
-        {isFetching && !isFetchingNextPage ? 'Background Updating...' : null}
-      </div>
-      <br />
-      <br />
-      {process.env.NODE_ENV === 'development' ? (
-        <p>
-          <strong>Notice:</strong> You are currently running React in
-          development mode. Rendering performance will be slightly degraded
-          until this application is build for production.
-        </p>
-      ) : null}
     </div>
   )
 }
