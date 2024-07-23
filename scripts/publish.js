abb03f26d9cbfc8b79f89f523293eec0dd8c6837
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-23 11:06:00 +1000

    ci: sync with other TanStack projects

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,12 +1,50 @@
 // @ts-check
 
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { publish } from '@tanstack/config/publish'
-import { branchConfigs, packages, rootDir } from './config.js'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 await publish({
-  branchConfigs,
-  packages,
-  rootDir,
+  packages: [
+    {
+      name: '@tanstack/lit-virtual',
+      packageDir: 'packages/lit-virtual',
+    },
+    {
+      name: '@tanstack/react-virtual',
+      packageDir: 'packages/react-virtual',
+    },
+    {
+      name: '@tanstack/solid-virtual',
+      packageDir: 'packages/solid-virtual',
+    },
+    {
+      name: '@tanstack/svelte-virtual',
+      packageDir: 'packages/svelte-virtual',
+    },
+    {
+      name: '@tanstack/virtual-core',
+      packageDir: 'packages/virtual-core',
+    },
+    {
+      name: '@tanstack/vue-virtual',
+      packageDir: 'packages/vue-virtual',
+    },
+  ],
+  branchConfigs: {
+    main: {
+      prerelease: false,
+    },
+    alpha: {
+      prerelease: true,
+    },
+    beta: {
+      prerelease: true,
+    },
+  },
+  rootDir: resolve(__dirname, '..'),
   branch: process.env.BRANCH,
   tag: process.env.TAG,
   ghToken: process.env.GH_TOKEN,
