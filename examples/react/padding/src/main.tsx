f28cd833d6c5dbc79d0d44462a3c1cb4eb0a09b9
Michael DeMarco <mdemarco@makenotion.com>
2025-03-07 00:27:10 -0800

    examples: "application is build" -> "application is built" (#909)
    * fix: typo in virtual examples
    
    * Fix other usages
    
    ---------
    
    Co-authored-by: Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>

--- examples/react/padding/src/main.tsx
+++ examples/react/padding/src/main.tsx
@@ -249,7 +249,7 @@ function GridVirtualizerDynamic({
         <p>
           <strong>Notice:</strong> You are currently running React in
           development mode. Rendering performance will be slightly degraded
-          until this application is build for production.
+          until this application is built for production.
         </p>
       ) : null}
     </>
