30bcf558bf2daaeb8972204e825d8f064b279ce3
Damian Pieczynski <piecyk@gmail.com>
2025-06-27 14:33:31 +0200

    chore(react-virtual): fix vite e2e build (#1030)

--- packages/react-virtual/e2e/app/vite.config.ts
+++ packages/react-virtual/e2e/app/vite.config.ts
@@ -1,7 +1,17 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: __dirname,
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@tanstack/react-virtual': path.resolve(__dirname, '../../src/index'),
+      '@tanstack/virtual-core': path.resolve(
+        __dirname,
+        '../../../virtual-core/src/index',
+      ),
+    },
+  },
 })
