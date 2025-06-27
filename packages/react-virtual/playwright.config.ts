3c759781f05f0aad2f5b7ba483539f9a5a7f8817
Damian Pieczynski <piecyk@gmail.com>
2025-06-27 13:31:14 +0200

    WIP

--- packages/react-virtual/playwright.config.ts
+++ packages/react-virtual/playwright.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from '@playwright/test'
 
 const PORT = 5173
+const baseURL = `http://localhost:${PORT}`
 
 export default defineConfig({
   testDir: './e2e/app/test',
   use: {
-    baseURL: `http://localhost:${PORT}`,
+    baseURL,
   },
   webServer: {
-    command: 'vite --config e2e/app/vite.config.ts',
-    port: PORT,
+    command: `VITE_SERVER_PORT=${PORT} vite build --config e2e/app/vite.config.ts && VITE_SERVER_PORT=${PORT} vite preview --config e2e/app/vite.config.ts --port ${PORT}`,
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
   },
