34d5fb01380f8e55a8f3db70e81e17abcbf40f31
dasathyakuma <dasathyakuma@ebay.com>
2026-04-10 15:13:29 -0600

    Fix examples

---
+++ examples/marko/grid/src/data.ts
@@ -0,0 +1,4 @@
+export const ROW_COUNT = 1000
+export const COL_COUNT = 1000
+export const rowSizes = Array.from({ length: ROW_COUNT }, () => 30 + Math.round(Math.random() * 20))
+export const colSizes = Array.from({ length: COL_COUNT }, () => 80 + Math.round(Math.random() * 80))
